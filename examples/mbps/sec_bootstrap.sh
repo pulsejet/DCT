3932aac7bdea87620433fccb9f3605010494a5b6
tylerliu <tylersiqi@163.com>
2021-08-24 01:31:53 -0700

    target rename

--- examples/mbps/sec_bootstrap.sh
+++ examples/mbps/sec_bootstrap.sh
@@ -3,7 +3,7 @@
     ../../tools/schema_cert -o mbpsDemo.schema mbpsDemo.scm mbpsDemo.root
     ../../tools/make_cert -s EdDSA -o alice.cert myNet/mbpsDemo/controller/alice mbpsDemo.root
     ../../tools/make_cert -s EdDSA -o bob.cert myNet/mbpsDemo/viewer/bob mbpsDemo.root
-    ../../tools/make_cert -s EdDSA -o frontdoor.cert myNet/mbpsDemo/gateway/frontdoor mbpsDemo.root
+    ../../tools/make_cert -s EdDSA -o California.cert myNet/mbpsDemo/gateway/California mbpsDemo.root
     ../../tools/make_bundle -o alice.bundle mbpsDemo.root mbpsDemo.schema +alice.cert
     ../../tools/make_bundle -o bob.bundle mbpsDemo.root mbpsDemo.schema +bob.cert
-    ../../tools/make_bundle -o frontdoor.bundle mbpsDemo.root mbpsDemo.schema +frontdoor.cert
+    ../../tools/make_bundle -o California.bundle mbpsDemo.root mbpsDemo.schema +California.cert
