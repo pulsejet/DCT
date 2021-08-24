da004e52b9eefde6fdcd4731386de8e592b0a782
tylerliu <tylersiqi@163.com>
2021-08-24 03:26:13 -0700

    use case schema

--- examples/mbps/sec_bootstrap.sh
+++ examples/mbps/sec_bootstrap.sh
@@ -4,6 +4,8 @@
     ../../tools/make_cert -s EdDSA -o alice.cert myNet/mbpsDemo/controller/alice mbpsDemo.root
     ../../tools/make_cert -s EdDSA -o bob.cert myNet/mbpsDemo/viewer/bob mbpsDemo.root
     ../../tools/make_cert -s EdDSA -o California.cert myNet/mbpsDemo/gateway/California mbpsDemo.root
+    ../../tools/make_cert -s EdDSA -o Texas.cert myNet/mbpsDemo/gateway/Texas mbpsDemo.root
     ../../tools/make_bundle -o alice.bundle mbpsDemo.root mbpsDemo.schema +alice.cert
     ../../tools/make_bundle -o bob.bundle mbpsDemo.root mbpsDemo.schema +bob.cert
     ../../tools/make_bundle -o California.bundle mbpsDemo.root mbpsDemo.schema +California.cert
+    ../../tools/make_bundle -o Texas.bundle mbpsDemo.root mbpsDemo.schema +Texas.cert
